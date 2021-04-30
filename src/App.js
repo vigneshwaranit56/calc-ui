45bfc99be39b3c28c5b421fea99b587d85f99943
deepika2001 <deepikajeenbaptista@gmail.com>
2021-04-30 09:31:53 +0530

    ClearHistory

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import ClearHistoryScreen from './ClearHistoryScreen.js';  
+
+
+
+function App(){
+  return(
+    <ClearHistoryScreen/>
+  );
+}
+export default App;
---
+++ src/ClearHistoryScreen.js
@@ -0,0 +1,18 @@
+import React  from 'react';
+import './ClearHistoryStyle.css';
+
+
+class ClearHistoryScreen extends React.Component{
+  render(){
+      
+    return(
+      <div className='App'>
+          <button type="reset">ClearHistory</button>
+      
+      </div>
+    );
+  
+  }
+ 
+}
+export default ClearHistoryScreen;
