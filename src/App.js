b7e75981a1689867993f0177954c1951d5e716b9
ilakkiya27 <kilakkiyakpm2018@gmail.com>
2021-05-04 04:38:33 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import ClearHistoryScreen from './ClearHistoryScreen.js'
+
+
+
+function App(){
+  return(
+    <ClearHistoryScreen/>
+  );
+}
+export default App;
+
+
+  
+
+
+
+
+
+
---
+++ src/ClearHistoryScreen.js
@@ -0,0 +1,20 @@
+
+import React  from 'react';
+import './ClearHistoryStyle.css';
+
+
+
+class ClearHistoryScreen extends React.Component{
+  render(){
+      
+    return(
+      <div className='App'>
+          <button type="reset">ClearHistory</button>
+      
+      </div>
+    );
+  
+  }
+ 
+}
+export default ClearHistoryScreen;
