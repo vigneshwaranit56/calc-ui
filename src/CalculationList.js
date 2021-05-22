2a843ccadf556bbf46045667fe09a7de77c29fd3
deepika2001 <deepikajeenbaptista@gmail.com>
2021-05-22 13:50:41 +0530

    init

--- src/CalculationList.js
+++ src/CalculationList.js
@@ -15,18 +15,37 @@ class CalculationList extends React.Component{
                         {model.operand1+"  "+model.operand2+" "+model.operation+" "+model.answer}
                              </li>
             );
+            
             return (
               <div>
                 {listItems}
               </div>
             );
           }
-          const models=[{"id": 1, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, {"id": 2, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, {"id": 3, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, {"id": 4, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, {"id": 5, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, {"id": 6, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, {"id": 7, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, {"id": 8, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, {"id": 9, "operand1": 8.0, "operand2": 100.0, "operation": "add", "answer": 108.0}, {"id": 10, "operand1": 8.0, "operand2": 100.0, "operation": "add", "answer": 108.0}, {"id": 11, "operand1": 8.0, "operand2": 200.0, "operation": "add", "answer": 208.0}, {"id": 12, "operand1": 8.0, "operand2": 200.0, "operation": "mul", "answer": 1600.0}, {"id": 13, "operand1": 8.0, "operand2": 200.0, "operation": "mul", "answer": 1600.0}, {"id": 14, "operand1": 8.0, "operand2": 200.0, "operation": "mul", "answer": 1600.0}, {"id": 15, "operand1": 8.0, "operand2": 200.0, "operation": "mul", "answer": 1600.0}, {"id": 16, "operand1": 8.0, "operand2": 200.0, "operation": "mul", "answer": 1600.0}, {"id": 17, "operand1": 8.0, "operand2": 200.0, "operation": "mul", "answer": 1600.0}];
-        
+          const models=[{"id": 1, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, 
+                        {"id": 2, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, 
+                        {"id": 3, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, 
+                        {"id": 4, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, 
+                        {"id": 5, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, 
+                        {"id": 6, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, 
+                        {"id": 7, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, 
+                        {"id": 8, "operand1": -6.0, "operand2": -100.0, "operation": "mul", "answer": 600.0}, 
+                        {"id": 9, "operand1": 8.0, "operand2": 100.0, "operation": "add", "answer": 108.0}, 
+                        {"id": 10, "operand1": 8.0, "operand2": 100.0, "operation": "add", "answer": 108.0}, 
+                        {"id": 11, "operand1": 8.0, "operand2": 200.0, "operation": "add", "answer": 208.0}, 
+                        {"id": 12, "operand1": 8.0, "operand2": 200.0, "operation": "mul", "answer": 1600.0}, 
+                        {"id": 13, "operand1": 8.0, "operand2": 200.0, "operation": "mul", "answer": 1600.0}, 
+                        {"id": 14, "operand1": 8.0, "operand2": 200.0, "operation": "mul", "answer": 1600.0}, 
+                        {"id": 15, "operand1": 8.0, "operand2": 200.0, "operation": "mul", "answer": 1600.0}, 
+                        {"id": 16, "operand1": 8.0, "operand2": 200.0, "operation": "mul", "answer": 1600.0}, 
+                        {"id": 17, "operand1": 8.0, "operand2": 200.0, "operation": "mul", "answer": 1600.0}];
+         
           return(
+            <ul>
             <div id ="models">
              <NumberList numbers={models}> </NumberList>
             </div>
+            </ul>
           );
     }
 
--- src/ClearHistoryScreen.js
+++ src/ClearHistoryScreen.js
@@ -3,19 +3,24 @@ import './ClearHistoryStyle.css';
 import CalculationList from './CalculationList.js';
 
 
+
+
+
 class ClearHistoryScreen extends React.Component{
  
        render() {
             return(
               <div className='App'>
-                <div id='d1'>
-                  <div>
-                <CalculationList> </CalculationList>
-                </div>
                 
-                </div>
-               <button type="reset">ClearHistory</button>
-               </div>
+           
+                
+                              <div id='d1'>
+                                <div>
+                              <CalculationList> </CalculationList>
+                              </div>
+                              </div>
+                                  <button type="reset">ClearHistory</button>
+                              </div>
                
             );  
         }
